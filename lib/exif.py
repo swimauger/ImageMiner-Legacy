ff5ff21c2b5cabe0ff1946123d46306d64bf9c36
swimauger <swimauger@gmail.com>
2020-10-13 11:49:54 -0500

    Add export exif

--- lib/exif.py
+++ lib/exif.py
@@ -1,17 +1,48 @@
 from .module.PIL import Image, ExifTags
+from .module.xlsxwriter import Workbook
+import sys
 
 def extract_exif(source):
     image = Image.open(source)
     exifdata = image.getexif()
 
-    print(source)
-    for tag_id in exifdata:
-        tag = ExifTags.TAGS.get(tag_id, tag_id)
-        data = exifdata.get(tag_id)
-        
-        if isinstance(data, bytes):
-            data = data.decode()
-        print(f"{tag:25}: {data}")
+    exportable_exif = { "ImageId": source }
 
-def export_exif(exif):
-    return {}
+    for tag_id in exifdata:
+        try:
+            tag = ExifTags.TAGS.get(tag_id, tag_id)
+            data = exifdata.get(tag_id)
+            if isinstance(data, bytes):
+                data = data.decode()
+            exportable_exif[tag] = data
+        except:
+            continue
+    return exportable_exif
+
+def get_headers(exif_images):
+    headers = {}
+    for exportable_exif in exif_images:
+        for header in exportable_exif:
+            if header not in headers:
+                headers[header] = True
+    
+    return list(headers.keys())
+
+def export_exif(exif_images):
+    wb = Workbook('/Users/mark/Desktop/python-imagemine.xlsx')
+    ws = wb.add_worksheet()
+
+    headers = get_headers(exif_images)
+    for col in range(len(headers)):
+        ws.write(0, col, headers[col])
+
+    row = 1
+    for exportable_exif in exif_images:
+        for col in range(len(headers)):
+            if headers[col] in exportable_exif:
+                ws.write(row, col, exportable_exif[headers[col]])
+            else:
+                ws.write(row, col, "")
+        row += 1
+    
+    wb.close()
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from lib.module import webview
 from lib.api import API
-from lib.exif import extract_exif
+from lib.exif import extract_exif, export_exif
 
 def init():
     api = API()
-    window = webview.create_window('Image Miner', 'src/index.html', js_api=api)
+    window = webview.create_window('Image Miner', 'app/index.html', js_api=api)
 
     def open_explorer(message):
         file_types = (['Image Files (*.png;*.jpg;*.jpeg;*.gif;*.bmp;*.svg)'])
         images = window.create_file_dialog(webview.OPEN_DIALOG, allow_multiple=True, file_types=file_types)
+        return images
+        
+    def export(images):
+        exif_images = []
         for image in images:
-            extract_exif(image)
+            exif_images.append(extract_exif(image))
+        export_exif(exif_images)
 
     api.on('open_explorer', open_explorer)
+    api.on('export', export)
 
     webview.start()
 
