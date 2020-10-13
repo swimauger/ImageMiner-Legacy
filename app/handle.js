21ac242a7c250ca11d5663f8d86311773d94c8fb
swimauger <swimauger@gmail.com>
2020-10-13 11:51:06 -0500

    Add support for uploaded images

--- app/handle.js
+++ app/handle.js
@@ -1,3 +1,28 @@
-document.querySelector('button').addEventListener('click', () => {
-    pywebview.api.send('open_explorer', null);
-});
+const $nofiles = document.querySelector('div.nofiles');
+const $files = document.querySelector('div.files');
+const $export = document.querySelector('button.export');
+
+async function uploadFiles() {
+    const response = await pywebview.api.send('open_explorer', null);
+    const images = JSON.parse(response)[0];
+    if (images.length) {
+        $nofiles.style.display = 'none';
+        $files.style.display = 'block';
+        $export.style.display = 'block';
+    }
+    for (const image of images) {
+        $files.innerHTML += `
+            <div class="file-item">
+                <div>
+                    <i class="fa fa-file"></i>
+                    <p>${image}</p>
+                </div>
+                <i class="fa fa-times"></i>
+            </div>
+        `;
+    }
+}
+
+document.querySelector('button.file-explorer').addEventListener('click', uploadFiles);
+
+document.querySelector('div.nofiles a').addEventListener('click', uploadFiles);
